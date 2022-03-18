ca52d4808a1755270ddd0911f2abb2e457439051
Ivan Fernandez Gracia <48660555+IvanFernandezGracia@users.noreply.github.com>
2022-03-18 20:19:15 -0300

    v14

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -3,25 +3,25 @@ const { isAuthenticated, hasRoles } = require("../auth");
 const router = express.Router();
 const Orders = require("../models/Orders");
 
-router.get("/",isAuthenticated, (req, res) => {
+router.get("/", isAuthenticated, (req, res) => {
   Orders.find()
     .exec()
     .then((x) => res.status(200).send(x));
 });
 
-router.get("/:id",isAuthenticated, (req, res) => {
+router.get("/:id", isAuthenticated, (req, res) => {
   Orders.findById(req.params.id)
     .exec()
     .then((x) => res.status(200).send(x));
 });
 
-router.post("/", isAuthenticated, hasRoles(["admin"]), (req, res) => {
+router.post("/", isAuthenticated, hasRoles(["user"]), (req, res) => {
   const { _id } = req.user;
   const { meal_id } = req.body;
   Orders.create({ meal_id, user_id: _id }).then((x) => res.status(201).send(x));
 });
 
-router.put("/:id", isAuthenticated, hasRoles(["admin", "user"]), (req, res) => {
+router.put("/:id", isAuthenticated, hasRoles(["admin"]), (req, res) => {
   //console.log(req.params.id, req.body);
   Orders.findByIdAndUpdate(req.params.id, req.body)
     .then((x) => res.status(204).send(x))
